161e4e2c760512e81ebd8987a23273b97650ab95
chosun-nhn18 <kimshh21@gmail.com>
2025-12-01 20:32:53 +0900

    application.yml config 서버에 맞게 수정

--- src/main/java/com/nhnacademy/memberapi/MemberApiApplication.java
+++ src/main/java/com/nhnacademy/memberapi/MemberApiApplication.java
@@ -2,12 +2,10 @@ package com.nhnacademy.memberapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableRedisRepositories
-@PropertySource("classpath:application-env.yml")
 public class MemberApiApplication {
 
     public static void main(String[] args) {
